35b2f0ab54e0a29f14b0444410474fa381cb0164
StygianLightning <denrellum@gmail.com>
2021-03-13 04:27:53 +0100

    Added exports for VecStorage and MapStorage.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@ mod no_such_entity;
 mod register;
 mod vecstorage;
 
-pub use crate::no_such_entity::NoSuchEntity;
 pub use entity::Entities;
 pub use entity::Entity;
+pub use mapstorage::MapStorage;
+pub use no_such_entity::NoSuchEntity;
 pub use register::Register;
+pub use vecstorage::VecStorage;
